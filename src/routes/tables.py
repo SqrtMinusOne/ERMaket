a1c41d691bcc56d6b910ac89a3e09fd9f80e66f8
SqrtMinusOne <thexcloud@gmail.com>
2019-11-19 12:28:44 +0300

    REST API works

--- src/routes/tables.py
+++ src/routes/tables.py
@@ -1,9 +1,23 @@
-from flask import abort, request
-import json
+from flask import abort, request, jsonify
+import simplejson as json
 
 from app import app, models
 from api.database import DBConn
 from api.models import NamesConverter
+from api.queries import QueryBuilder
+
+
+def get_filter_params(args, pagination=True):
+    kwargs = {}
+    kwargs['filter_by'] = json.loads(
+        request.args.get('filter_by', default='[]', type=str))
+
+    if pagination:
+        kwargs['page'] = request.args.get('page', default=1, type=int)
+        kwargs['per_page'] = request.args.get('per_page', default=10, type=int)
+        kwargs['order_by'] = json.loads(
+            request.args.get('order_by', default='[]', type=str))
+    return kwargs
 
 
 @app.route('/table/<schema>/<table>')
@@ -12,5 +26,21 @@ def get_table(schema, table):
         model = models[schema][NamesConverter.table_to_class(schema, table)]
     except KeyError:
         abort(404)
-    filter = json.loads(request.args.get('filter', default='{}', type=str))
-    page = request.args.get('page', default=0, type=int)
+    kwargs = get_filter_params(request.args)
+    with DBConn.get_session() as session:
+        builder = QueryBuilder(session)
+        result = builder.fetch_data(model, **kwargs)
+    return jsonify(result)
+
+
+@app.route('/entry/<schema>/<table>')
+def get_entry(schema, table):
+    try:
+        model = models[schema][NamesConverter.table_to_class(schema, table)]
+    except KeyError:
+        abort(404)
+    kwargs = get_filter_params(request.args, pagination=False)
+    with DBConn.get_session() as session:
+        builder = QueryBuilder(session)
+        result = builder.fetch_one(model, **kwargs)
+    return jsonify(result)
