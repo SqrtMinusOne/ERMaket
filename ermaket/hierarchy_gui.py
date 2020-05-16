f2e8ea72f7e6c06081ccd056d04505da954c04f3
SqrtMinusOne <thexcloud@gmail.com>
2020-05-16 13:33:39 +0300

    Omit depSecondWishes in get all depSchWishes

--- ermaket/hierarchy_gui.py
+++ ermaket/hierarchy_gui.py
@@ -1,5 +1,7 @@
 import logging.config
+import os
 import sys
+from pathlib import Path
 
 from PyQt5.QtWidgets import QApplication
 
@@ -9,6 +11,11 @@ from ermaket.ui import HierachyEditor
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     config = Config()
+    Path(
+        os.path.dirname(
+            config.Logging['handlers']['file_handler']['filename']
+        )
+    ).mkdir(parents=True, exist_ok=True)
     logging.config.dictConfig({**config.Logging, **config.GUILogging})
 
     editor = HierachyEditor()
